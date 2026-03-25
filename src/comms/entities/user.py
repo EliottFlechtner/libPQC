c404826efc918db6757882810425aaaa4692275a
EliottFlechtner <eliott.flechtner@epita.fr>
2026-03-25 15:29:06 -0500

    refactor: remove unused Channel class and delete empty schnorr.py and pke.py files

--- src/comms/entities/user.py
+++ src/comms/entities/user.py
@@ -5,12 +5,3 @@ class User:
 
     def __str__(self):
         return f"User(id={self.user_id}, username='{self.username}')"
-
-
-class Channel:
-    def __init__(self, channel_id: int, name: str):
-        self.channel_id = channel_id
-        self.name = name
-
-    def __str__(self):
-        return f"Channel(id={self.channel_id}, name='{self.name}')"
